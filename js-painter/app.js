7fbcbcf120e4a7c08a0fcc8f8a69d229c5eb1d02
miso <suhon_@naver.com>
2022-07-05 00:53:20 +0900

    선그리기 작동

--- js-painter/app.js
+++ js-painter/app.js
@@ -1,9 +1,58 @@
-const canvas = document.getElementsById("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-function onMouseMove(event){
-    console.log(event);
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    //console.log(event);
+    const x = event.offsetX;
+    const y = event.offsetY;
+    //console.log(x, y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+
+}
+
+function onMouseDown(event){
+ //console.log(event);   
+ painting = true;
+}
+/*
+function onMouseUp(event){
+    //painting = false;
+    stopPainting();
+}
+*/
+/*
+function onMouseLeave(event){
+    painting = false;
+}
+*/
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
+    //canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
+    //canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
+    //canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
